9f38f111fefd112796c018be9124577da46576cd
PnLament <fengyunyi2007@126.com>
2026-02-27 13:53:55 +0800

    feast-抽象线性表.h

---
+++ List.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <bits/stdc++.h>
+
+/// @brief 顺序表
+/// @tparam T 元素类型
+template <class T>
+class list
+{
+    public:
+        virtual void clear() = 0; // 清空顺序表
+        list(){};
+        virtual ~list(){};
+        virtual int length() const = 0;
+        virtual void insert(int i, const T &x) = 0;
+        virtual void remove(int i) = 0;
+        virtual int search(const T &x) const = 0;
+        virtual T visit(int i) const = 0;
+        virtual void traverse() const = 0;
+        
+};
